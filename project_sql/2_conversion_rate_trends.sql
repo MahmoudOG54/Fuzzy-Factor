b0927c9500e645d8ba92a08f094ec390bc8d3b99
mahmoud elsayed <me4012956@gmail.com>
2026-07-20 16:06:42 +0300

    update coversion_rate

--- project_sql/2_conversion_rate_trends.sql
+++ project_sql/2_conversion_rate_trends.sql
@@ -5,5 +5,16 @@ What is the session-to-order conversion rate? How has it trended over time?
 Purpose:
 To evaluate how effectively the website converts visitors into paying customers month-over-month or week-over-week.
 */
-
--- Write your SQL query below:
+    SELECT
+    DATE_TRUNC('month',website_sessions.created_at)::DATE AS create_month,
+   count(DISTINCT website_sessions.website_session_id)AS total_vists,
+    COUNT( DISTINCT orders.order_id) AS total_orders,
+    ROUND(
+             COUNT( DISTINCT orders.order_id) * 100.0 /COUNT(DISTINCT website_sessions.website_session_id),
+             2
+    ) AS conv_rate
+    FROM website_sessions
+    LEFT JOIN orders ON website_sessions.website_session_id = orders.website_session_id
+    GROUP BY 1
+    ORDER BY 1
+    LIMIT 50;
